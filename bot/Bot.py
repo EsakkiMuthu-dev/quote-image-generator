fb94ead782f9edf9c7cbaedee48069cfa9713a58
Rasla <programmer.rasla@gmail.com>
2022-05-20 23:46:11 +0530

    Instagram Upload Config Done

--- bot/Bot.py
+++ bot/Bot.py
@@ -1,25 +1,26 @@
 from email.policy import strict
 import imp
 from io import BytesIO
+import os
 from posixpath import split
 import quopri
 from PIL import Image, ImageDraw, ImageFont
 from .config import Config
 import requests
 from .Utils import Utils
-
+import instapy_cli
 class Bot():
 
     def __init__(self, config) -> None:
         """Automatically post a random image and a random programming quote to the instagram."""
         self.config = config
         self.utils = Utils()
+        self.instapy = instapy_cli.client(username=self.config.getEnvVariable("username"), password=self.config.getEnvVariable("password"))
 
     def getRandomImage(self) -> Image:
         """Get a random image from the instagram."""
         url = self.config.getVariable('randomImageUrl')
 
-        # Get the image
         response = requests.get(url)
         img = Image.open(BytesIO(response.content))
 
@@ -42,43 +43,52 @@ class Bot():
         color = (0,0,0) if self.utils.findImageIsDarkOrBright(Image.open(imagepath)) else (255,255,255)
 
         if(self.utils.findImageIsDarkOrBright(Image.open(imagepath))):
-            # make the image little bit more bright
             image = Image.open(imagepath)
             image = image.point(lambda x: min(x + 100, 255))
             image.save(imagepath)
         else:
-            # make the image little bit more dark
             image = Image.open(imagepath)
             image = image.point(lambda x: max(x - 100, 0))
             image.save(imagepath)
 
-
-        # Add the quote to the image
         img = Image.open(imagepath)
         draw = ImageDraw.Draw(img)
-        print(self.config.returnFontPath())
-        # load the custom font
+
         font = ImageFont.truetype(self.config.returnFontPath(), size=fontsize)
 
         draw.text((x1 / 2, y1 / 2), formatted_quote, font=font, fill=color, anchor="mm")
-        # write name on the botom of the image
+
         fontForName = ImageFont.truetype(self.config.retrunFontForName(), size=15)
         draw.text((x1 /2, y1 - 10 ), "Lazy Insta Bot \U0001f600 Coded and Managed By @rassouniqz...follow for more posts", font=fontForName, fill=color, anchor="ms")
+        img.save("bot/img.jpg")
         img.save('img.jpg')
 
     def PostImagetoInsta(self):
         """Post the image to the instagram."""
-        # Get the image
+
+        # get parent directory of the script
+        try:
+            imagepath = os.path.join(os.path.dirname(__file__), "img.jpg")
+            print(imagepath)
+            session = self.instapy
+
+            self.instapy.upload(imagepath, 
+                caption=self.utils.formatTheString(self.getRandomProgrammingQuote()['en']) + "\n\n- " + self.getRandomProgrammingQuote()['author'],
+            )
         
+            os.remove(imagepath)
+            print("Posted to instagram")
+        except Exception as e:
+            print(e)
+            print("Failed to post to instagram")
 
     def main(self) -> None:
         """Main function for the bot."""
-        # open the image
-
         
         quote = self.getRandomProgrammingQuote()['en']
         author = self.getRandomProgrammingQuote()['author']
 
         self.createImage(quote=quote, author=author)
+        self.PostImagetoInsta()
         
         
