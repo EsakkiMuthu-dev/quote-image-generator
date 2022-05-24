054cd6ab1df7f4f50184ca6ba7c2a898675eff7d
Rasla <programmer.rasla@gmail.com>
2022-05-24 10:29:05 +0530

    Changes made in run py

--- run.py
+++ run.py
@@ -7,7 +7,6 @@ schedule.every(5).hours.do(bot.main)
 
 while True:
     schedule.run_pending()
-    os.system("cls")
-    time.sleep(7200)
+    os.system("clear")
     if(os.path.isfile("config/rassouniqz_uuid_and_cookie.json")):
         os.remove("config/rassouniqz_uuid_and_cookie.json")
