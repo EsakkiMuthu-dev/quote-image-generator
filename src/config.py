3fa16752c8c8b5bd56101f3dde62ba6439939b42
EsakkiMuthu <86913985+EsakkiMuthu-dev@users.noreply.github.com>
2023-05-24 10:21:16 +0530

    added moviepy in requirements.txt and added comments

--- src/config.py
+++ src/config.py
@@ -1,11 +1,11 @@
 import os
 
-
 class Config:
     def __init__(self):
         pass
 
     def get(self, key):
+        # Get the value for the specified key from the configuration dictionary
         config = {
             "image_apiurl": "https://source.unsplash.com/1080x1080/?nature",
             "quotable_apiurl": "https://api.quotable.io/random",
@@ -16,12 +16,17 @@ class Config:
         return config[key]
 
     def loadfont_path(self, type):
+        # Load the path of the font based on the specified type
         if type == "main":
+            # Return the path of the main font
             return os.path.join("fonts", "Anton-Regular.ttf")
+        # Return the path of the default font
         return os.path.join("fonts", "OpenSans-Variable.ttf")
 
     def loadimage_path(self):
+        # Load the path of the image
         return os.path.join(os.getcwd(), "image.png")
     
     def loadmusic_path(self):
+        # Load the path of the music file
         return os.path.join(os.getcwd(), "sudari.mp3")
